080a27b0a018e1d36e90d62a823918bcfc76c44f
Anatoly <foxes20.01.01@gmail.com>
2022-07-27 19:26:46 +0700

    avtoLoadClass

--- index.php
+++ index.php
@@ -5,20 +5,29 @@ $path = __DIR__.'/'.'scripts/'.$url.'.php';
 // echo $url."<br>";
 // echo $path."<br>";
 
-if (file_exists($path)) {
-    require_once($path);
-} else {
-    require_once('./scripts/error404.php');
-}
+//if (file_exists($path)) {
+//    require_once($path);
+//} else {
+//    require_once('./scripts/error404.php');
+//}
 
 
+
+
+spl_autoload_register(function ($className) {
+    require_once (__DIR__.'/'.'scripts/'.$className.'.php');
+
+    if (!class_exists($className, false)) {
+        throw new LogicException("Unable to load class: $className");
+    }
+});
 
 if (class_exists($url)) {
-    $o = new ($url);
+    $o = new $url;
     $o->run();
 } else {
     $o = new error404;
-    $o->run(error404);
+    $o->run();
 }
 
 
