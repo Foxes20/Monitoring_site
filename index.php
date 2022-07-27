ae344f11a46e91b42df3d3b96a33a82bbb61faac
Anatoly <foxes20.01.01@gmail.com>
2022-07-27 14:59:37 +0700

    routing class

--- index.php
+++ index.php
@@ -1,17 +1,39 @@
 <?php
-
 $url = $_GET['url'];
-
 $path = __DIR__.'/'.'scripts/'.$url.'.php';
 
 // echo $url."<br>";
 // echo $path."<br>";
 
 if (file_exists($path)) {
-
-    require_once(__DIR__.'/'.'scripts/'.$url.'.php');
-
+    require_once($path);
 } else {
     require_once('./scripts/error404.php');
 }
+
+
+
+if (class_exists($url)) {
+    $o = new ($url);
+    $o->run();
+} else {
+    $o = new error404;
+    $o->run(error404);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
